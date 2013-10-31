106c93bb42acf6a20a1fafa409a036f8158e729b
Villiard <sim_villiard@hotmail.com>
2013-10-31 13:01:26 -0400

    Backwash

--- GARMU/Modele/GarmuModel.Designer.cs
+++
@@ -1,4 +0,0 @@
-﻿// La génération de code par défaut est désactivée pour le modèle 'D:\Documents\GitHub\GARMU\GARMU\Modele\GarmuModel.edmx'. 
-// Pour l'activer, modifiez la valeur du concepteur 'Stratégie de génération de code'.
-// Cette propriété est disponible dans la fenêtre Propriétés, lorsque le modèle est ouvert
-// dans le concepteur.
